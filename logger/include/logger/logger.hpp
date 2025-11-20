99964d23afb2296256cee314b62eca3a9dbc8381
dronxxxy <dronxxxy@gmail.com>
2025-11-20 06:52:22 +0300

    formatting

--- logger/include/logger/logger.hpp
+++ logger/include/logger/logger.hpp
@@ -11,19 +11,23 @@ namespace logger {
     virtual void log(Level level, std::string_view string) = 0;
 
   public:
-    template<typename ... Args> void error(std::string_view message, Args ...args) {
+    template<typename ... Args>
+    void error(std::string_view message, Args ...args) {
       this->log(Level::Error, std::vformat(message, std::make_format_args(args...)));
     }
 
-    template<typename ... Args> void warning(std::string_view message, Args ...args) {
+    template<typename ... Args>
+    void warning(std::string_view message, Args ...args) {
       this->log(Level::Warning, std::vformat(message, std::make_format_args(args...)));
     }
 
-    template<typename ... Args> void info(std::string_view message, Args ...args) {
+    template<typename ... Args>
+    void info(std::string_view message, Args ...args) {
       this->log(Level::Info, std::vformat(message, std::make_format_args(args...)));
     }
 
-    template<typename ... Args> void debug(std::string_view message, Args ...args) {
+    template<typename ... Args>
+    void debug(std::string_view message, Args ...args) {
       this->log(Level::Debug, std::vformat(message, std::make_format_args(args...)));
     }
   };
