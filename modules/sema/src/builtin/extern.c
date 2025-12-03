b6e9e97dbc65af30e3539405ce956584961d1ea3
dronxxxy <dronxxxy@gmail.com>
2025-12-03 08:17:39 +0300

    fixed const assertions

--- modules/sema/src/builtin/extern.c
+++ modules/sema/src/builtin/extern.c
@@ -2,6 +2,7 @@
 #include "const.h"
 #include "plugin.h"
 #include "sema.h"
+#include "type.h"
 #include "value.h"
 
 lou_sema_value_t *lou_extern_builtin(lou_sema_plugin_call_ctx_t *ctx) {
@@ -10,6 +11,6 @@ lou_sema_value_t *lou_extern_builtin(lou_sema_plugin_call_ctx_t *ctx) {
   lou_slice_t *name = LOU_SEMA_EXPECT_NOT_NULL(ctx->sema, ctx->arg_slices[0], lou_sema_value_is_const_string(ctx->args[0]), "expected constant");
   lou_sema_type_t *type = LOU_SEMA_EXPECT_NOT_NULL(ctx->sema, ctx->arg_slices[1], lou_sema_value_is_type(ctx->args[1]), "expected type");
 
-  return lou_sema_value_new_constant(ctx->sema->mempool, lou_sema_const_new_extern(ctx->sema->mempool, type, *name));
+  return lou_sema_value_new_constant(ctx->sema->mempool, lou_sema_const_new_extern(ctx->sema->mempool, lou_sema_type_new_pointer(ctx->sema->mempool, type), *name));
 }
 
--- modules/sema/src/const.c
+++ modules/sema/src/const.c
@@ -1,8 +1,9 @@
 #include "const.h"
+#include "type.h"
 #include <assert.h>
 
 lou_sema_const_t *lou_sema_const_new_func(lou_mempool_t *mempool, lou_sema_type_t *type, lou_hir_func_t *func) {
-  assert(type->kind == LOU_SEMA_TYPE_FUNC);
+  assert(type->kind == LOU_SEMA_TYPE_FUNCTION);
   lou_sema_const_t *constant = LOU_MEMPOOL_ALLOC(mempool, lou_sema_const_t);
   constant->kind = LOU_SEMA_CONST_FUNCTION;
   constant->func = func;
@@ -10,7 +11,7 @@ lou_sema_const_t *lou_sema_const_new_func(lou_mempool_t *mempool, lou_sema_type_
 }
 
 lou_sema_const_t *lou_sema_const_new_integer(lou_mempool_t *mempool, lou_sema_type_t *type, uint64_t integer) {
-  assert(type->kind == LOU_SEMA_TYPE_INT);
+  assert(type->kind == LOU_SEMA_TYPE_INTEGER);
   lou_sema_const_t *constant = LOU_MEMPOOL_ALLOC(mempool, lou_sema_const_t);
   constant->kind = LOU_SEMA_CONST_INTEGER;
   constant->integer = integer;
@@ -26,7 +27,7 @@ lou_sema_const_t *lou_sema_const_new_string(lou_mempool_t *mempool, lou_sema_typ
 }
 
 lou_sema_const_t *lou_sema_const_new_extern(lou_mempool_t *mempool, lou_sema_type_t *type, lou_slice_t name) {
-  assert(type->kind == LOU_SEMA_TYPE_STRING);
+  assert(type->kind == LOU_SEMA_TYPE_POINTER);
   lou_sema_const_t *constant = LOU_MEMPOOL_ALLOC(mempool, lou_sema_const_t);
   constant->kind = LOU_SEMA_CONST_EXTERN;
   constant->external_name = name;
